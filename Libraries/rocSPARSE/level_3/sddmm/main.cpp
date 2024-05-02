ee2702dc33217cdbfe902f182f511bd0803bb488
Beatriz Navidad Vilches <61422851+Beanavil@users.noreply.github.com>
2024-05-02 13:20:38 +0200

    rocSPARSE examples (general functions, part II) (#102)
    * rocSPARSE level 3 spsm example
    
    * rocSPARSE level 2 spmv example
    
    * rocSPARSE level 3 sddmm example
    
    * Add rocSPARSE level 3 spmm example
    
    * Move spsm from 5.5 to 5.7
    
    * Move spmv from 5.5 to 5.7
    
    * Move sddmm from 5.5 to 5.7
    
    * Move spmm from 5.5 to 5.7
    
    ---------
    
    Co-authored-by: Bence Parajdi <bence@streamhpc.com>
    Co-authored-by: Mátyás Aradi <matyas@streamhpc.com>

---
+++ Libraries/rocSPARSE/level_3/sddmm/main.cpp
@@ -0,0 +1,262 @@
+// MIT License
+//
+// Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "rocsparse_utils.hpp"
+
+#include <rocsparse/rocsparse.h>
+
+#include <hip/hip_runtime.h>
+
+#include <array>
+#include <iomanip>
+#include <iostream>
+
+int main()
+{
+    // 1. Set up input data.
+    // Number of rows and columns of the input matrices.
+    constexpr rocsparse_int m = 6;
+    constexpr rocsparse_int k = 4;
+    constexpr rocsparse_int n = 8;
+
+    // Dense matrix A (m x k)
+    //     (  9  8 12  9 )
+    // A = ( 24 13 11 10 )
+    //     (  6 27 15  6 )
+    //     ( 21  3 15  0 )
+    //     ( 10 18 36  0 )
+    //     ( 17  6 12 27 )
+
+    // Matrix A elements.
+    // clang-format off
+    std::array<double, (m * k)> h_A
+        = { 9,  8, 12,  9,
+           24, 13, 11, 10,
+            6, 27, 15,  6,
+           21,  3, 15,  0,
+           10, 18, 36,  0,
+           17,  6, 12, 27};
+    // clang-format on
+
+    // Dense matrix B (k x n).
+    //     (  9 11   13   15 17 10 12 14 )
+    // B = (  8 10    1   10  6 11  7  3 )
+    //     ( 12  6    0   18 12  9 12 15 )
+    //     (  9 27   18   36 27 18 27  9 )
+
+    // Matrix B elements.
+    // clang-format off
+    std::array<double, (k * n)> h_B
+        = { 9, 11, 13, 15, 17, 10, 12, 14,
+            8, 10,  1, 10,  6, 11,  7,  3,
+           12,  6,  0, 18, 12,  9, 12, 15,
+            9, 27, 18, 36, 27, 18, 27,  9};
+    // clang-format on
+
+    // Sparse matrix C (m x n).
+    //     ( 1  2  0  3  0  0  0  0 )
+    // C = ( 0  4  5  0  0  0  0  0 )
+    //     ( 0  0  6  7  8  0  0  0 )
+    //     ( 0  0  0  9 10 11  0 12 )
+    //     ( 0 13  0  0 14  0  0  0 )
+    //     ( 0  0 15  0  0 16  0  0 )
+    // Transposition of the matrix
+
+    // Number of non-zero entries.
+    constexpr rocsparse_int nnz = 16;
+
+    // CSR values.
+    std::array<double, nnz> h_csr_val
+        = {1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16.};
+
+    // CSR column indices.
+    std::array<rocsparse_int, nnz> h_csr_col_ind = {0, 1, 3, 1, 2, 2, 3, 4, 3, 4, 5, 7, 1, 4, 2, 5};
+
+    // CSR row indices.
+    std::array<rocsparse_int, (m + 1)> h_csr_row_ptr = {0, 3, 5, 8, 12, 14, nnz};
+
+    // Transposition of the matrix.
+    constexpr rocsparse_order     order   = rocsparse_order_row;
+    constexpr rocsparse_operation trans_A = rocsparse_operation_none;
+    constexpr rocsparse_operation trans_B = rocsparse_operation_none;
+
+    // Scalars alpha and beta.
+    constexpr double alpha = 1.0;
+    constexpr double beta  = 1.0;
+
+    // Temporary buffer.
+    size_t buffer_size   = 0;
+    void*  d_temp_buffer = nullptr;
+
+    // Index and data type.
+    constexpr rocsparse_indextype index_type = rocsparse_indextype_i32;
+    constexpr rocsparse_datatype  data_type  = rocsparse_datatype_f64_r;
+
+    // Index base.
+    constexpr rocsparse_index_base index_base = rocsparse_index_base_zero;
+
+    // 2. Prepare device for calculation.
+    // rocSPARSE handle
+    rocsparse_handle handle;
+    ROCSPARSE_CHECK(rocsparse_create_handle(&handle));
+
+    // 3. Allocate device memory and offload data to device.
+    double*        d_csr_val;
+    rocsparse_int* d_csr_col_ind;
+    rocsparse_int* d_csr_row_ptr;
+    double*        d_A;
+    double*        d_B;
+
+    constexpr size_t size_val     = sizeof(*d_csr_val) * nnz;
+    constexpr size_t size_col_ind = sizeof(*d_csr_col_ind) * nnz;
+    constexpr size_t size_row_ind = sizeof(*d_csr_row_ptr) * (m + 1);
+    constexpr size_t size_A       = sizeof(*d_A) * m * k;
+    constexpr size_t size_B       = sizeof(*d_B) * k * n;
+
+    HIP_CHECK(hipMalloc(&d_csr_val, size_val));
+    HIP_CHECK(hipMalloc(&d_csr_col_ind, size_col_ind));
+    HIP_CHECK(hipMalloc(&d_csr_row_ptr, size_row_ind));
+    HIP_CHECK(hipMalloc(&d_A, size_A));
+    HIP_CHECK(hipMalloc(&d_B, size_B));
+
+    HIP_CHECK(hipMemcpy(d_csr_val, h_csr_val.data(), size_val, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(d_csr_col_ind, h_csr_col_ind.data(), size_col_ind, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(d_csr_row_ptr, h_csr_row_ptr.data(), size_row_ind, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(d_A, h_A.data(), size_A, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(d_B, h_B.data(), size_B, hipMemcpyHostToDevice));
+
+    // 4. Create matrix descriptors.
+    // Matrix descriptor.
+    rocsparse_dnmat_descr mat_A_desc{};
+    rocsparse_dnmat_descr mat_B_desc{};
+    rocsparse_spmat_descr mat_C_desc{};
+
+    // Create sparse csr descriptor.
+    rocsparse_create_csr_descr(&mat_C_desc,
+                               m,
+                               n,
+                               nnz,
+                               d_csr_row_ptr,
+                               d_csr_col_ind,
+                               d_csr_val,
+                               index_type,
+                               index_type,
+                               index_base,
+                               data_type);
+
+    // Create dense descriptors.
+    rocsparse_create_dnmat_descr(&mat_A_desc, m, k, k, d_A, data_type, order);
+    rocsparse_create_dnmat_descr(&mat_B_desc, k, n, n, d_B, data_type, order);
+
+    // 5. Calculate size and allocate temp buffer.
+    ROCSPARSE_CHECK(rocsparse_sddmm_buffer_size(handle,
+                                                trans_A,
+                                                trans_B,
+                                                &alpha,
+                                                mat_A_desc,
+                                                mat_B_desc,
+                                                &beta,
+                                                mat_C_desc,
+                                                data_type,
+                                                rocsparse_sddmm_alg::rocsparse_sddmm_alg_default,
+                                                &buffer_size));
+
+    HIP_CHECK(hipDeviceSynchronize());
+
+    // Allocate the temp buffer.
+    HIP_CHECK(hipMalloc(&d_temp_buffer, buffer_size));
+
+    // 6. Do the necessary pre calculations.
+    // The result is written in the temp buffer.
+    ROCSPARSE_CHECK(rocsparse_sddmm_preprocess(handle,
+                                               trans_A,
+                                               trans_B,
+                                               &alpha,
+                                               mat_A_desc,
+                                               mat_B_desc,
+                                               &beta,
+                                               mat_C_desc,
+                                               data_type,
+                                               rocsparse_sddmm_alg::rocsparse_sddmm_alg_default,
+                                               d_temp_buffer));
+
+    // 7. Compute the solution.
+    ROCSPARSE_CHECK(rocsparse_sddmm(handle,
+                                    trans_A,
+                                    trans_B,
+                                    &alpha,
+                                    mat_A_desc,
+                                    mat_B_desc,
+                                    &beta,
+                                    mat_C_desc,
+                                    data_type,
+                                    rocsparse_sddmm_alg::rocsparse_sddmm_alg_default,
+                                    d_temp_buffer));
+
+    HIP_CHECK(hipDeviceSynchronize());
+
+    // 8. Copy C to host from device.
+    HIP_CHECK(hipMemcpy(h_csr_val.data(), d_csr_val, size_val, hipMemcpyDeviceToHost));
+    HIP_CHECK(hipMemcpy(h_csr_col_ind.data(), d_csr_col_ind, size_col_ind, hipMemcpyDeviceToHost));
+    HIP_CHECK(hipMemcpy(h_csr_row_ptr.data(), d_csr_row_ptr, size_row_ind, hipMemcpyDeviceToHost));
+
+    // 9. Clear rocSPARSE.
+    ROCSPARSE_CHECK(rocsparse_destroy_handle(handle));
+    ROCSPARSE_CHECK(rocsparse_destroy_dnmat_descr(mat_A_desc));
+    ROCSPARSE_CHECK(rocsparse_destroy_dnmat_descr(mat_B_desc));
+    ROCSPARSE_CHECK(rocsparse_destroy_spmat_descr(mat_C_desc));
+
+    // 10. Clear device memory.
+    HIP_CHECK(hipFree(d_temp_buffer));
+    HIP_CHECK(hipFree(d_csr_val));
+    HIP_CHECK(hipFree(d_csr_col_ind));
+    HIP_CHECK(hipFree(d_csr_row_ptr));
+    HIP_CHECK(hipFree(d_A));
+    HIP_CHECK(hipFree(d_B));
+
+    // 11. Print result.
+    std::cout << "C =" << std::endl;
+
+    for(rocsparse_int i = 0; i < m; ++i)
+    {
+        int row_pos = h_csr_row_ptr[i];
+        int row_end = h_csr_row_ptr[i + 1];
+
+        std::cout << "    (";
+        for(rocsparse_int j = 0; j < n; ++j)
+        {
+            if(row_pos < row_end && j == h_csr_col_ind[row_pos])
+            {
+                std::cout << std::setw(5) << h_csr_val[row_pos];
+                ++row_pos;
+            }
+            else
+            {
+                std::cout << std::setw(5) << 0;
+            }
+        }
+
+        std::cout << " )" << std::endl;
+    }
+
+    return 0;
+}
---
+++ Libraries/rocSPARSE/level_3/spsm/README.md
@@ -0,0 +1,153 @@
+# rocSPARSE Level 3 Triangular Solver Example
+## Description
+This example illustrates the use of the `rocSPARSE` level 3 triangular solver with a chosen sparse format.
+
+The operation solves the following equation for $X$:
+
+$A' \cdot X = \alpha \cdot B'$
+
+where
+
+- given a matrix $M$, $M'$ denotes one of the following:
+    - $M' = M$ (identity)
+    - $M' = M^T$ (transpose $M$: $M_{ij}^T = M_{ji}$)
+    - $M' = M^H$ (conjugate transpose/Hermitian $M$: $M_{ij}^H = \bar M_{ji}$),
+- $A$ is a sparse triangular matrix of order $m$ in CSR or COO format,
+- $X$ is a dense matrix of size $m\times n$ containing the unknowns of the system,
+- $B$ is a dense matrix of size $m\times n$ containing the right hand side of the equation,
+- $\alpha$ is a scalar
+
+## Application flow
+1. Set up a sparse matrix in CSR format. Allocate matrices $B$ and $C$ and set up the scalar $\alpha$.
+2. Prepare device for calculation.
+3. Allocate device memory and copy input matrices from host to device.
+4. Create matrix descriptors.
+5. Do the calculation.
+6. Copy the result matrix from device to host.
+7. Clear rocSPARSE allocations on device.
+8. Clear device memory.
+9. Print result to the standard output.
+
+## Key APIs and Concepts
+### CSR Matrix Storage Format
+The [Compressed Sparse Row (CSR) storage format](https://rocsparse.readthedocs.io/en/latest/usermanual.html#csr-storage-format) describes an $m \times n$ sparse matrix with three arrays.
+
+Defining
+- `m`: number of rows
+- `n`: number of columns
+- `nnz`: number of non-zero elements
+
+we can describe a sparse matrix using the following arrays:
+- `csr_val`: array storing the non-zero elements of the matrix.
+- `csr_row_ptr`: given $i \in [0, m]$
+    - if $` 0 \leq i < m `$, `csr_row_ptr[i]` stores the index of the first non-zero element in row $i$ of the matrix
+    - if $i = m$, `csr_row_ptr[i]` stores `nnz`.
+
+    This way, row $j \in [0, m)$ contains the non-zero elements of indices from `csr_row_ptr[j]` to `csr_row_ptr[j+1]-1`. Therefore, the corresponding values in `csr_val` can be accessed from `csr_row_ptr[j]` to `csr_row_ptr[j+1]-1`.
+- `csr_col_ind`: given $i \in [0, nnz-1]$, `csr_col_ind[i]` stores the column of the $i^{th}$ non-zero element in the matrix.
+
+The CSR matrix is sorted by column indices in the same row, and each pair of indices appear only once.
+
+For instance, consider a sparse matrix as
+
+$$
+A=
+\left(
+\begin{array}{ccccc}
+1 & 2 & 0 & 3 & 0 \\
+0 & 4 & 5 & 0 & 0 \\
+6 & 0 & 0 & 7 & 8
+\end{array}
+\right)
+$$
+
+Therefore, the CSR representation of $A$ is:
+
+```
+m = 3
+
+n = 5
+
+nnz = 8
+
+csr_val = { 1, 2, 3, 4, 5, 6, 7, 8 }
+
+csr_row_ptr = { 0, 3, 5, 8 }
+
+csr_col_ind = { 0, 1, 3, 1, 2, 0, 3, 4 }
+```
+
+### rocSPARSE
+- `rocsparse_spsm(...)` performs a sparse matrix-dense matrix multiplication. This single function is used to run all three stages of the calculation.
+   - `rocsparse_spsm_stage_buffer_size` will query the size of the temporary buffer, which will hold the data between the preprocess and the compute stages.
+   - `rocsparse_spsm_stage_preprocess` will preprocess the data and save it in the temporary buffer.
+   - `rocsparse_spsm_stage_compute` will do the actual spsm calculation.
+   - `rocsparse_spsm_stage_auto` will figure out the current stage and run it. If `temp_buffer` pointer equals nullptr the stage will be `rocsparse_spsm_stage_buffer_size`, if `buffer_size` the function will perform the `rocsparse_spsm_stage_preprocess` stage, otherwise it will perform the `rocsparse_spsm_stage_compute`.
+   - The `rocsparse_spsm_stage_buffer_size` and `rocsparse_spsm_stage_compute` stages are asynchronous. It is the callers responsibility to assure that these stages are complete, before using their result. `rocsparse_spsm_stage_preprocess` will run in a blocking manner, no synchronization is necessary.
+
+- `rocsparse_spsm_alg`: list of SpSM algorithms.
+    - `rocsparse_spsm_alg_default`: default SpSM algorithm for the given format (the only available option)
+
+- `rocsparse_operation`: matrix operation type with the following options:
+   - `rocsparse_operation_none`: identity operation: $A' = A$
+   - `rocsparse_operation_transpose`: transpose operation: $A' = A^\mathrm{T}$
+   - `rocsparse_operation_conjugate_transpose`: Hermitian operation: $A' = A^\mathrm{H}$. This is currently not supported.
+
+- `rocsparse_datatype`: data type of rocSPARSE vector and matrix elements.
+    - `rocsparse_datatype_f32_r`: real 32-bit floating point type
+    - `rocsparse_datatype_f64_r`: real 64-bit floating point type
+    - `rocsparse_datatype_f32_c`: complex 32-bit floating point type
+    - `rocsparse_datatype_f64_c`: complex 64-bit floating point type
+    - `rocsparse_datatype_i8_r`: real 8-bit signed integer
+    - `rocsparse_datatype_u8_r`: real 8-bit unsigned integer
+    - `rocsparse_datatype_i32_r`: real 32-bit signed integer
+    - `rocsparse_datatype_u32_r` real 32-bit unsigned integer
+
+- `rocsparse_indextype` indicates the index type of a rocSPARSE index vector.
+    - `rocsparse_indextype_u16`: 16-bit unsigned integer
+    - `rocsparse_indextype_i32`: 32-bit signed integer
+    - `rocsparse_indextype_i64`: 64-bit signed integer
+
+- `rocsparse_index_base` indicates the index base of indices.
+    - `rocsparse_index_base_zero`: zero based indexing.
+    - `rocsparse_index_base_one`: one based indexing.
+
+## Demonstrated API Calls
+### rocSPARSE
+- `rocsparse_create_csr_descr`
+- `rocsparse_create_dnmat_descr`
+- `rocsparse_create_handle`
+- `rocsparse_datatype`
+- `rocsparse_datatype_f64_r`
+- `rocsparse_destroy_dnmat_descr`
+- `rocsparse_destroy_handle`
+- `rocsparse_destroy_spmat_descr`
+- `rocsparse_dnmat_descr`
+- `rocsparse_handle`
+- `rocsparse_spmat_descr`
+- `rocsparse_index_base`
+- `rocsparse_index_base_zero`
+- `rocsparse_indextype`
+- `rocsparse_indextype_i32`
+- `rocsparse_int`
+- `rocsparse_operation`
+- `rocsparse_operation_none`
+- `rocsparse_order`
+- `rocsparse_order_column`
+- `rocsparse_spmat_descr`
+- `rocsparse_spsm`
+- `rocsparse_spsm_alg`
+- `rocsparse_spsm_alg_default`
+- `rocsparse_spsm_stage`
+- `rocsparse_spsm_stage_buffer_size`
+- `rocsparse_spsm_stage_compute`
+- `rocsparse_spsm_stage_preprocess`
+
+### HIP runtime
+- `hipDeviceSynchronize`
+- `hipFree`
+- `hipMalloc`
+- `hipMemcpy`
+- `hipMemcpyDeviceToHost`
+- `hipMemcpyHostToDevice`
+- `hipMemset`
