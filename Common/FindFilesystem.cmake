08f1052d59ee456553d7e70ec2b9be350f67a138
zichguan-amd <zichuan.guan@amd.com>
2025-09-08 10:21:07 -0700

    Support experimental filesystem header for GCC versions in [5.3, 8.1) (#307)
    Use experimental/filesystem for older GCC versions.
    Skip reduction tutorial by checking for execution header.
    
    Signed-off-by: zichguan-amd <zichuan.guan@amd.com>
    (cherry picked from commit d61371871fad30ce9936074271a02403cfca4164)

---
+++ Common/FindFilesystem.cmake
@@ -0,0 +1,79 @@
+# MIT License
+#
+# Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+cmake_minimum_required(VERSION 3.21 FATAL_ERROR)
+
+include(CMakePushCheckState)
+include(CheckIncludeFileCXX)
+include(CheckCXXSourceCompiles)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+cmake_push_check_state(RESET)
+
+# find available header: filesystem or experimental/filesystem
+check_include_file_cxx("filesystem" CXX_HAVE_FS_HEADER)
+if (CXX_HAVE_FS_HEADER)
+    set(FS_HEADER filesystem)
+    set(FS_NAMESPACE std::filesystem)
+    set(CXX_FS_HEADER_FOUND ON)
+else()
+    check_include_file_cxx("experimental/filesystem" CXX_HAVE_EXPERIMENTAL_FS_HEADER)
+    if(CXX_HAVE_EXPERIMENTAL_FS_HEADER)
+        set(FS_HEADER experimental/filesystem)
+        set(FS_NAMESPACE std::experimental::filesystem)
+        set(CXX_FS_HEADER_FOUND ON)
+    else()
+        set(CXX_FS_HEADER_FOUND OFF)
+        return()
+    endif()
+endif()
+
+string(CONFIGURE
+[[
+    #include <@FS_HEADER@>
+    #include <iostream>
+
+    int main() {
+        std::cout << @FS_NAMESPACE@::current_path() << std::endl;
+        return 0;
+    }
+]] test_code @ONLY)
+
+# Check if filesystem works without additional libraries
+check_cxx_source_compiles("${test_code}" CXX_FS_NO_LINK)
+
+if (NOT CXX_FS_NO_LINK)
+    # Check if we can link stdc++fs
+	set(CMAKE_REQUIRED_LIBRARIES stdc++fs)
+	check_cxx_source_compiles("${test_code}" CXX_FS_CAN_LINK)
+    if (CXX_FS_CAN_LINK)
+        set(CXX_FS_LIBRARY stdc++fs CACHE STRING "Additional library required to use filesystem" FORCE)
+    endif()
+endif()
+
+cmake_pop_check_state()
+
+if(NOT CXX_FS_NO_LINK AND NOT CXX_FS_CAN_LINK)
+    message(FATAL_ERROR "Cannot run simple program using filesystem")
+endif()
