0ea85c235bf37c73b0597aeec089aaea8660c981
Jan Stephan <jan.stephan@amd.com>
2026-02-18 14:05:32 +0100

    Add missing pointer cast
    Signed-off-by: Jan Stephan <jan.stephan@amd.com>

--- Libraries/rocProfiler-SDK/external_correlation_id_request/main.cpp
+++ Libraries/rocProfiler-SDK/external_correlation_id_request/main.cpp
@@ -323,7 +323,7 @@ void run_migrate(int rank, int tid, hipStream_t stream, int, char** argv)
     }
 
     auto page_data_dev_ptr = static_cast<data_type*>(nullptr);
-    HIP_CHECK(hipHostGetDevicePointer(&page_data_dev_ptr, page_data.data(), 0));
+    HIP_CHECK(hipHostGetDevicePointer(reinterpret_cast<void**>(&page_data_dev_ptr), page_data.data(), 0));
 
     test_page_migrate<<<1, 1024, 0, stream>>>(page_data_dev_ptr, incr_v);
 
